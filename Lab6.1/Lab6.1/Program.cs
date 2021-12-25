5fb0430fa3d8312d894dcc78a6d05a8a586b9c43
kvo-52 <kvo.52@mail.ru>
2021-12-25 18:07:23 +0300

    *

---
+++ Lab6.1/Lab6.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Домашнее задание  6.1 Ввести с клавиатуры предложение.
+           // Предложение представляет собой слова, разделенные пробелом.
+          // Знаки препинания не используются.Найти самое длинное слово в строке.
+            Console.WriteLine();
+            // Вводим фразу с клавиатуры
+            string stringStart = Console.ReadLine();
+            Console.WriteLine();
+            // Разбиваем фразу на слова
+            string[] stingArray = stringStart.Split();
+            // Считаем количество слов в предложении
+            int SumWord = 0;
+            foreach (string s in stingArray)
+            {
+                SumWord++;
+            }
+            Console.WriteLine("Количество слов в предложении равно {0}", SumWord);
+            // Создаем массив из длин слов и находим самое длинное слово
+            int[] arrayLength = new int[SumWord];
+            int max = 0;
+            foreach (string s in stingArray)
+            {
+                int n = Convert.ToInt32(s.Length);
+                if (n > max)
+                    max = n;
+            }
+            Console.WriteLine("Количество знаков в самом длинном слове равно {0}", max);
+            Console.WriteLine();
+            // Выводим саме длинные слова
+            Console.Write("Самое длинное слово: ");
+            int colWord = 0;
+            foreach (string s in stingArray)
+            {
+                int n = Convert.ToInt32(s.Length);
+                if (n == max)
+                {
+                    colWord++;
+                    Console.Write("{0} ", s);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Для завершения программы нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab8.2/Lab8.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab8._2
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "logs2/log.txt";
+            StreamWriter sw = new StreamWriter(parh, v);
+            sw.WriteLine("Старт программы");
+            sw.WriteLine("Ошибка");
+            sw.Flush();
+        }
+    }
+}
